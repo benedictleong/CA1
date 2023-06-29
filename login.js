4b92ef67ec6fccdeb88b5158b0d1c0e17838d625
benedictleong <bleon903@gmail.com>
2023-06-29 18:12:31 +0800

    pwd

--- login.js
+++ login.js
@@ -1,13 +1,13 @@
-var input = require('readline-sync');
+function login() {
+	var username = document.getElementById("username").value;
+	var password = document.getElementById("password").value;
 
-function checkpassword() {
-    var user = document.getElementsById('username');
-    var pswd = document.getElementById(password);
-
-    if(user === 'admin' && pswd === 'adminuser') {
-        alert("You have successfully logged in.");
-        location.reload(index.html);
-    } else {
-        alert("Password or Username is incorrect.")
-    }
+	// Perform validation
+	if (username == "" || password == "") {
+		alert("Please enter your username and password");
+	} else if (username != "admin" || password != "password") {
+		alert("Invalid username or password");
+	} else {
+		alert("Login successful");
+	}
 }
